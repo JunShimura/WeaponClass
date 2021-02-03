3d1bfd9974135d6f3d0d207d45de7b0f537b7b59
Jun Shimura <junshimura@gmail.com>
2021-02-03 16:31:29 +0900

    1st

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float hoge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MonoIn>();        //〇　Monoから派生したclassはAddComponent
+        //MonoIn monoInTest = new MonoIn();          //✖ 実行時に警告が出る、Start()は動かない
+        //gameObject.AddComponent<NoMono>();    //✖エラーでビルド不可
+        NoMono noMonoInTest = new NoMono();        //〇 Monoから派生していないclassはnew
+    }
+
+
+    float pastTime = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Weapon[] weapon = GetComponents<Weapon>();
+            foreach (Weapon w in weapon)
+            {
+                w.Shoot();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    protected string weaponName;        // 名前を保持するメンバ変数
+    protected uint maxTimes;                  //使用できる回数
+    protected uint lastTimes;              //残りの使用回数
+    protected uint expendTimes;          //一度の使用で消費する使用回数
+    protected uint expend()
+    {
+        lastTimes -= expendTimes;
+        return lastTimes;
+    }
+    public abstract void Shoot();   // 武器を使用するメンバ関数
+    string getName()
+    {
+        return name;
+    }
+    protected  void  Init(string name, uint maxTimes, uint expendTimes)    // コンストラクタ
+    {
+        this.weaponName = name;
+        this.maxTimes = maxTimes;
+        this.expendTimes = expendTimes;
+        lastTimes = maxTimes;
+    }
+
+}
